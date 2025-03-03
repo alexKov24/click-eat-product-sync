6588e986ed2003461255e59708a61940c51eda8f
Alex <56908393+alexKov24@users.noreply.github.com>
2025-03-03 13:59:25 +0200

    changed versioning system

--- click-eat-product-sync.php
+++ click-eat-product-sync.php
@@ -3,7 +3,7 @@
 Plugin Name: ClickEat Products Sync
 Plugin URI: https://webchad.tech
 Description: Syncs products, categories and subcategories from ClickEat API
-Version: 2025.02.25.3
+Version: 0.0.2
 Author: Alex Kovalev
 */
 
